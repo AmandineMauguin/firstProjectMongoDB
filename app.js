ca809f7a2a528761fa66e7378afdedf8f7f2fbf2
Amandine Mauguin <greenies@mbp-de-amandine-1.home>
2021-03-17 15:01:33 +0100

    ajout Expressjs

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
+//Tout en haut ce sont les imports 
 const mongoose = require('mongoose');
+const express = require('express')
+
+const app = express();
+app.listen(3000);
+app.get('/', (req, res) => {
+    res.send('Bjr ça marche')
+  })
+
+
 
 const URI ="mongodb+srv://admin:Mongo21&@cluster0.x67oc.mongodb.net/sample_airbnb?retryWrites=true&w=majority"
 mongoose.connect(URI, {useNewUrlParser: true,
@@ -22,8 +32,18 @@ count.exec((err, result) => {
     console.log('Resultat count : '+ result);
 })
 
-//Affiche les 5 premiers noms de la liste 
-const listing= myModel.find({}, {name:1}).limit(5);
-listing.exec((err, result)=> {
-    console.log(result);
+//On affiche les résultats de la requête mongo sur le serveur Express à l'adresse /api
+const listing = myModel.find({name: /a/}, {_id:1, name:1, listing_url:1}).skip(10).limit(20).sort({_id:1});
+app.get('/api', (request, response) => {
+    listing.exec((err, result) => {
+        console.log(result);
+        response.send(result);
+    })
 })
+
+
+//Affiche les 5 premiers noms de la liste en sipant les 10 premiers 
+// const listing= myModel.find({name:/a/}, {name:1}).skip(10).sort({_id:1});
+// listing.exec((err, result)=> {
+//     console.log(result);
+// })
